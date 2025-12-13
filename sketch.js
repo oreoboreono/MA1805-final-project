a9c704fe13c807e38ddab22506d529cab79f613f
OreoB <oreoboreon@gmail.com>
2025-12-13 22:15:04 +0000

    s

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
-function setup() {
-  createCanvas(3540, 1860);
+function preload(){
+  mainMenuGIF = loadGif('assets/screens/menuScreen');
 }
+
+
+function setup() {
+  createCanvas(3840, 2160);
+  mainMenuGIF.play();
+}
+
+
 
 
 
 function draw() {
   background(0);
+  image(mainMenuGIF, 0, 0);
 }
